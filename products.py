5cb8c45661fd25916e3b4ef3a114007de8096b0f
dainalkai <120482248+dainalkai@users.noreply.github.com>
2022-12-16 13:48:49 +0800

    Add files via upload

--- products.py
+++ products.py
@@ -1,8 +1,28 @@
 products = []
+with open('product.csv', 'r', encoding='utf-8') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue # 繼續
+		name, price = line.strip().split(',') # strip去除換行, split這鬥後地方切
+		products.append([name, price])
+print(products)		
+
+#讓使用者輸入
 while True:
 	name = input('請輸入商品名稱:')
 	if name == 'q':
 		break
 	price = input('請輸入商品價格:')
+	price = int(price)
 	products.append([name, price])
-print(products)		
+print(products)
+
+#印出所有購買紀錄
+for p in products:
+	print(p[0])
+
+#寫入檔案
+with open('product.csv', 'w', encoding='utf-8') as f:
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n')	
